b2424490be9d3f2b7140fa1762297b398e7418d0
umerch1 <umerf3024@gmail.com>
2024-03-18 19:22:09 +0500

    PED Editing backend api

---
+++ app.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+require('./helpers/init_mongodb')
+const express = require("express");
+const morgan= require('morgan');
+const createError=require('http-errors');
+const app=express();
+// const cors=require("cors");
+// require("./db/conn")
+const AuthRoute=require('./Routes/Auth.route');
+const PORT=process.env.PORT || 6005;
+app.use(morgan('dev'))
+// app.use(cors({
+//     origin:"http://localhost:5173/",
+//     methods:"GET,POST,PUT,DELETE",
+//     credentials:true
+// }));
+
+app.get('/',async(req,res,next)=>{
+    res.send("Hello from express")
+})
+app.use('/auth',AuthRoute);
+app.use(async (rec,res,next)=>{
+    next(createError.NotFound())
+});
+app.use((err,req,res,next)=>{
+    res.status(err.status || 500)
+    res.send({
+        error:{
+            status:err.status ||500,
+            message :err.message,
+        }
+    })
+})
+// app.use(express.json());
+
+// app.get("/",(req,res)=>{
+//     res.status(200).json("server start")
+// });
+
+app.listen(PORT,()=>{
+    console.log(`Server start at a port no ${PORT}`)
+});
